714b58ac2bc1e8a3ff59ce54ddc1bc6ec9bc4f16
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-07-07 14:31:06 +0900

    extract fetch operation to method

--- scripts/addresser.rb
+++ scripts/addresser.rb
@@ -3,12 +3,10 @@ class Addresser
     query = env["QUERY_STRING"]
     return unprocessable_entity if query.empty?
 
-    postalcode = query.split("=")[1]
-    return unprocessable_entity if postalcode.nil?
+    postcode = query.split("=")[1]
+    return unprocessable_entity if postcode.nil?
 
-    url = "https://postcode-jp.appspot.com/api/postcode?general=true&office=false&postcode=#{postalcode}"
-    address = http_request(url).join[2].join
-    [200, {"content-type" => "application/json"}, [address]]
+    [200, {"content-type" => "application/json"}, [fetch(postcode)]]
   end
 
   private
@@ -16,6 +14,11 @@ class Addresser
   def unprocessable_entity
     res = "invalid request".to_json
     [422, {"content-type" => "application/json"}, [res]]
+  end
+
+  def fetch(postcode)
+    url = "https://postcode-jp.appspot.com/api/postcode?general=true&office=false&postcode=#{postcode}"
+    address = http_request(url).join[2].join
   end
 end
 
