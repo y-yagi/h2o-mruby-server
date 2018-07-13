7a5e9e0258e15041bebd039e4b10b5322b1394c0
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-07-13 16:39:27 +0900

    fix test method name

--- test/access_controler_test.rb
+++ test/access_controler_test.rb
@@ -1,10 +1,12 @@
 require "../scripts/access_controler"
 
 class AccessControllerTest < MTest::Unit::TestCase
-  def test_call
+  def test_access_allowed
     result = AccessController.new.call({"REMOTE_ADDR" => "172.17.0.1"})
     assert_equal [399, {}, []], result
+  end
 
+  def test_access_forbidden
     result = AccessController.new.call({"REMOTE_ADDR" => "127.0.0.1"})
     assert_equal [403, {'content-type' => 'text/plain'}, ["access forbidden\n"]], result
   end
